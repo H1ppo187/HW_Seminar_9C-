85792e6c3317d40b490c36662bd6f7e31bd463a1
Alexander Balashov <balashov187@yandex.ru>
2023-04-05 19:27:05 +0300

    Задача 68

--- Program.cs
+++ Program.cs
@@ -21,17 +21,33 @@
 // M = 4; N = 8. -> 30
 
 
+// Console.Write("Введите число M: ");
+// int M = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(SumNumbers(M, N));
+ 
+// int SumNumbers(int M, int N)
+// {
+//     if (M == 0) return (N * (N + 1)) / 2;            
+//     else if (N == 0) return (M * (M + 1)) / 2;     
+//     else if (M == N) return M;                       
+//     else if (M < N) return N + SumNumbers(M, N - 1); 
+//     return N + SumNumbers(M, N + 1);            
+// }
+// Задача 68: Выполнить с помощью рекурсии.Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
 Console.Write("Введите число M: ");
 int M = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine()!);
-Console.WriteLine(SumNumbers(M, N));
- 
-int SumNumbers(int M, int N)
+Console.WriteLine(AkkerFun(M, N));
+
+int AkkerFun(int M, int N)
 {
-    if (M == 0) return (N * (N + 1)) / 2;            
-    else if (N == 0) return (M * (M + 1)) / 2;     
-    else if (M == N) return M;                       
-    else if (M < N) return N + SumNumbers(M, N - 1); 
-    return N + SumNumbers(M, N + 1);            
+if (M == 0) return N + 1;
+else if (N == 0) return AkkerFun(M - 1, 1);
+return AkkerFun(M - 1, AkkerFun(M, N - 1));
 }
